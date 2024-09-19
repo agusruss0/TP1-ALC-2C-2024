08b939fb63deea3a64620e8be1ab316e1b62a5e7
agus <agustin.russo.dm@gmail.com>
2024-09-19 18:53:52 -0300

    wip

--- template-alumnos/funciones.py
+++ template-alumnos/funciones.py
@@ -54,43 +54,14 @@ def inversaLU(L, U):
 
     return Inv
 
+def inversaLUpivot(L,U,P):
+    return inversaLU(L,U) @ P
+
 
 def solve_LU(L, U, b):
     y = sp.linalg.solve_triangular(L, b, lower=True)
     x = sp.linalg.solve_triangular(U, y)
     return x
-
-
-def descompPLU(A):
-    n = len(A)
-    P = np.eye(n)
-    L = np.eye(n)
-    U = A.copy()
-
-    for i in range(n - 1):
-        # Buscar el índice del pivote máximo en la columna actual
-        max_index = np.argmax(abs(U[i:, i])) + i
-
-        if max_index != i:
-            # Intercambiar filas en la matriz de permutación P
-            P[[i, max_index]] = P[[max_index, i]]
-
-            # Intercambiar filas en la matriz U
-            U[[i, max_index]] = U[[max_index, i]]
-
-            # Intercambiar filas en la matriz L (anterior)
-            if i >= 1:
-                L[[i, max_index], :i] = L[[max_index, i], :i]
-
-        pivot = U[i, i]
-        if pivot == 0:
-            print("Tiene pivote nulo")
-            return None
-
-        L[i + 1 :, i] = U[i + 1 :, i] / pivot
-        U[i + 1 :, i:] -= np.outer(L[i + 1 :, i], U[i, i:])
-
-    return P, L, U
 
 
 A = np.array([[1, -1, 0, 1], [0, 1, 4, 0], [2, -1, 0, -2], [-3, 3, 0, -1]])
@@ -99,11 +70,14 @@ id = np.eye(rows, cols)
 
 leontief = id - A
 L, U, P = calcularPLU(leontief)
+print(leontief)
 print(L)
 print(U)
 print(P)
-print(L @ U)
+print("A permutada:\n", L @ U)
 print(np.linalg.inv(P))
-A_permutada = L @ U
-print(np.transpose(P) @ A_permutada)
-print(leontief)
+pa_inversa = inversaLU(L,U)
+print("A permutada inversa: \n", pa_inversa)
+A_inversa = pa_inversa @ P
+print("A inversa: \n", A_inversa)
+print("Leontief:\n", np.linalg.inv(leontief))
